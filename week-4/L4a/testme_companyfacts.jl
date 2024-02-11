66cb4fb6b66aa32982b3920c202afcd038c9c180
Jeffrey Varner <jdv27@cornell.edu>
2024-02-11 07:17:58 -0500

    Update testme_companyfacts.jl
    working on API

--- week-4/L4a/testme_companyfacts.jl
+++ week-4/L4a/testme_companyfacts.jl
@@ -2,7 +2,7 @@
 include("Include.jl")
 
 # build an endpoint model -
-model = MyEGDARCompanyFactsEndpointModel(cik = "0000320193"); # test AAPL
+model = MyEGDARCompanyFactsEndpointModel(cik = lpad("2488",10,'0')); # test AMD
 
 # build the URL -
 url_string = url("https://data.sec.gov", model);
