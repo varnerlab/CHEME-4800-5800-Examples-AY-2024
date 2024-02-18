8e325a4ef02f7f20155fd7d7c1d8e5ad34038e1a
Jeffrey Varner <jdv27@cornell.edu>
2024-02-18 16:29:27 -0500

    L5a
    L5a

---
+++ week-5/L5a/Include.jl
@@ -0,0 +1,10 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src")
+
+# if we are missing any packages, install them -
+using Pkg
+Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
+# load external package -
+using DataStructures
