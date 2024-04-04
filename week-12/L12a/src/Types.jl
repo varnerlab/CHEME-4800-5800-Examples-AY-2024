5809ee2619cb10fbe5caf4dd459c42f5cc138011
Jeffrey Varner <jdv27@cornell.edu>
2024-04-04 10:40:29 -0400

    Dual and Primal
    primal and dual code working

--- week-12/L12a/src/Types.jl
+++ week-12/L12a/src/Types.jl
@@ -51,3 +51,16 @@ mutable struct MyLinearProgrammingProblemModel <: AbstractLinearProgrammingProbl
     # constructor
     MyLinearProgrammingProblemModel() = new();
 end
+
+mutable struct MyLinearProgrammingDualProblemModel <: AbstractLinearProgrammingProblemType
+    
+    # data -
+    A::Array{Float64,2}     # constraint matrix
+    b::Array{Float64,1}     # right-hand side vector
+    c::Array{Float64,1}     # objective function coefficient vector
+    lb::Array{Float64,1}    # lower bound vector
+    ub::Array{Float64,1}    # upper bound vector
+
+    # constructor
+    MyLinearProgrammingDualProblemModel() = new();
+end
