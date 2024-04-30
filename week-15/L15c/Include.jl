0be92ba3eb17adc2189cedf14ff618d7b365dac7
Jeffrey Varner <jdv27@cornell.edu>
2024-04-30 13:56:08 -0400

    working
    working

---
+++ week-15/L15c/Include.jl
@@ -0,0 +1,19 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_IMAGES = joinpath(_ROOT, "images");
+
+
+# load external packages -
+using Pkg
+Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
+# using statements -
+using Images
+using ImageInTerminal
+using Flux
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
