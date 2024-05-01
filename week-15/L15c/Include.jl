8dd18edb287d3f9997f59f9e7564b9da93c243d4
Jeffrey Varner <jdv27@cornell.edu>
2024-05-01 05:43:56 -0400

    hmmm
    hmmm

--- week-15/L15c/Include.jl
+++ week-15/L15c/Include.jl
@@ -12,8 +12,12 @@ Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 using Images
 using ImageInTerminal
 using Flux
+using FileIO
+using OneHotArrays
+using Statistics
 
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"));
 include(joinpath(_PATH_TO_SRC, "Factory.jl"));
 include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
