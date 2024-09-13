e0b8cde7d8dd988dbd65ccc1cf0c5d46f63a94c5
Jeffrey Varner <jdv27@cornell.edu>
2024-09-13 12:50:05 -0400

    update
    update

--- week-12/L12c/Include.jl
+++ week-12/L12c/Include.jl
@@ -4,8 +4,13 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src")
 const _PATH_TO_DATA = joinpath(_ROOT, "data")
 
 # check for packages, if not installed, install them
-using Pkg
-Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
 
 # load packages -
 using GLPK
